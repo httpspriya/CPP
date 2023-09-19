158c75556a424b19eefa5718910c16e1ba54164f
Priyanshi Sondagar <122718112+httpspriya@users.noreply.github.com>
2023-09-19 14:58:31 +0530

    Add files via upload

---
+++ Assignment-2/ASS_2_10.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Distance{
+	int km;
+	long int m;
+	public:
+		void getData(){
+			cout<<"\nEnter following values:";
+			cout<<"\nKilometer:";
+			cin>>km;
+			cout<<"Meter:";
+			cin>>m;
+		}
+		void putData(){
+			cout<<"\nDetails:"<<endl;
+			cout<<"Kilometer   :"<<km<<endl;
+			cout<<"Meter       :"<<m;
+		}
+        Distance operator -(Distance d){
+            Distance ans;
+            ans.km=km-d.km;
+            ans.m=m-d.m;
+            return ans;
+        }
+};
+int main(){
+	Distance d1,d2,d3;
+	d1.getData();
+	d2.getData();
+	cout<<"\n\nData of object 1:"<<endl;
+	d1.putData();
+	cout<<"\n\nData of object 2:"<<endl;
+	d2.putData();
+	d3=d1-d2;
+	cout<<"\nData of after subtraction of object 1 and 2:"<<endl;
+	d3.putData();
+	return 0;
+}
